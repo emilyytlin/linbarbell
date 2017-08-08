275e51b6d59e0f095d115ed87c7014b8477c752a
Emily Lin <emilyytlin@yahoo.com>
2017-08-07 23:25:05 -0400

    - get rid of skeleton.css - save wilks formula image

--- static/js/wilks.js
+++ static/js/wilks.js
@@ -23,8 +23,11 @@
 
   var WilksViewModel = function() {
     this.wilks = new Wilks('kg', 'F');
+    this.m = { 'a': -216.0475144, 'b': 16.2606339, 'c': -0.002388645, 'd': -0.00113732, 'e': 7.01863E-06, 'f': -1.291E-08};
+    this.f = {'a': 594.31747775582, 'b': -27.23842536447, 'c': 0.82112226871, 'd': -0.00930733913, 'e': 4.731582E-05, 'f': -9.054E-08 };
     this.results = ko.observableArray();
     this.showInfo = ko.observable(false);
+    this.displayCoeff = function(c) { return 'a = ' + c.a + '\nb = ' + c.b + '\nc = ' + c.c + '\nd = ' + c.d + '\ne = ' + c.e + '\nf = ' + c.f; };
   };
 
   var round = function(value, decimals) {
@@ -43,9 +46,7 @@
     }
     total = total === 0 ? this.wilks.total() : total;
     var kglb = 2.2046226218;
-    var m = { 'a': -216.0475144, 'b': 16.2606339, 'c': -0.002388645, 'd': -0.00113732, 'e': 7.01863E-06, 'f': -1.291E-08};
-    var f = {'a': 594.31747775582, 'b': -27.23842536447, 'c': 0.82112226871, 'd': -0.00930733913, 'e': 4.731582E-05, 'f': -9.054E-08 };
-    var c = this.wilks.sex() === 'F' ? f : m;
+    var c = this.wilks.sex() === 'F' ? this.f : this.m;
     var bwKg = this.wilks.unit() === 'kg' ? this.wilks.bw() : this.wilks.bw() / kglb;
     var totalKg = this.wilks.unit() === 'kg' ? total : total / kglb;
     var wilks = 500 * totalKg / (c.a + c.b * bwKg + c.c * Math.pow(bwKg, 2) + c.d * Math.pow(bwKg, 3) + c.e * Math.pow(bwKg, 4) + c.f * Math.pow(bwKg, 5));
